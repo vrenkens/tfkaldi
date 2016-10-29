e4357cd8207b4e53477027b95ae3bec80e571d7c
v0lta <moritzwolter@gmx.de>
2016-10-29 15:48:55 +0200

    premerge las branch.

--- saved/load_las.py
+++ saved/load_las.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 epoch_loss_lst, epoch_loss_lst_val, \
 tl, LEARNING_RATE, MOMENTUM, OMEGA  \
- = pickle.load(open( "savedValsLastAdam.spchcl24.esat.kuleuven.be.pkl", "rb" ))
+ = pickle.load(open( "savedValsLastAdam.spchcl22.esat.kuleuven.be2016-10-28 07:19:11.626192.pkl", "rb" ))
 
 
 plt.plot(epoch_loss_lst)
