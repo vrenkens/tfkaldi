pBlstm layer sequence length computation bugfix.

las_main.py
@@ -55,10 +55,9 @@ LEARNING_RATE_DECAY = 1
 MOMENTUM = 0.9
 #OMEGA = 0.000 #weight regularization term.
 OMEGA = 0.001 #weight regularization term.
-INPUT_NOISE_STD = 0.6
 #LEARNING_RATE = 0.0001       #too low?
 #MOMENTUM = 0.6              #play with this.
-MAX_N_EPOCHS = 900
+MAX_N_EPOCHS = 600
 OVERFIT_TOL = 99999
 
 ####Network Parameters
@@ -121,15 +120,14 @@ las_trainer = LasTrainer(
 
 
 
-print("--- Tree generation done. --- time since start [min]",
-     (time.time() - start_time)/60.0)
+print('\x1b[01;32m' + "--- Graph generation done. --- time since start [min]",
+     (time.time() - start_time)/60.0, '\x1b[0m')
 
 ####Run session
 epoch = 0
 epoch_loss_lst = []
 epoch_loss_lst_val = []
 
-print("Graph done, starting computation.")
 las_trainer.start_visualization('log')
 #start a tensorflow session
 config = tf.ConfigProto()
@@ -138,13 +136,18 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 with tf.Session(graph=las_trainer.graph, config=config):
     #initialise the trainer
-    print('Initializing')
+    print("Initializing")
     las_trainer.initialize()
-
+    print("Starting computation.")
     inputs, targets = train_dispenser.get_batch()
     eval_loss = las_trainer.evaluate(inputs, targets)
     epoch_loss_lst.append(eval_loss)
-    print('pre evaluation loss:', eval_loss)
+    print("pre training loss:", eval_loss)
+
+    inputs, targets = val_dispenser.get_batch()
+    validation_loss = las_trainer.evaluate(inputs, targets)
+    epoch_loss_lst_val.append(validation_loss)
+    print('validation set pre training loss:', validation_loss)
 
     continue_training = True
     while continue_training:
@@ -156,13 +159,17 @@ with tf.Session(graph=las_trainer.graph, config=config):
         epoch = epoch + 1
 
         if epoch%5 == 0:
-            print("-----  validation Step --- time since start [min]   ",
-                  (time.time() - start_time)/60.0)
+            print('\x1b[01;32m'
+                  + "-----  validation Step --- time since start [h]   ",
+                  (time.time() - start_time)/3600.0,
+                  '\x1b[0m')
             epoch_loss_lst.append(train_loss)
 
             inputs, targets = val_dispenser.get_batch()
             validation_loss = las_trainer.evaluate(inputs, targets)
-            print("-----  validation loss: ", validation_loss)
+            print('\x1b[01;32m'
+                  + "-----  validation loss: ", validation_loss,
+                  '\x1b[0m')
             epoch_loss_lst_val.append(validation_loss)
 
         # if the training error is lower than the validation error for
@@ -185,17 +192,24 @@ with tf.Session(graph=las_trainer.graph, config=config):
 
         if epoch > MAX_N_EPOCHS:
             continue_training = False
-            print("stopping the training.")
+            print('\x1b[01;31m', "stopping the training.", '\x1b[0m')
+
+    print('saving the model')
+    today = str(datetime.datetime.now()).split(' ')[0]
+    filename = "saved_models/" \
+               + socket.gethostname() \
+               + '-' + today \
+               + ".mdl"
+    las_trainer.save_model(filename)
 
     #run the network on the test data set.
     inputs, targets = test_dispenser.get_batch()
     test_loss = las_trainer.evaluate(inputs, targets)
     print("test loss: ", test_loss)
 
-now = datetime.datetime.now()
 filename = "saved/savedValsLasAdam." \
            + socket.gethostname() \
-           + str(now) \
+           + '-' + today \
            + ".pkl"
 pickle.dump([epoch_loss_lst, epoch_loss_lst_val, test_loss, LEARNING_RATE,
              MOMENTUM, OMEGA, epoch], open(filename, "wb"))
@@ -205,5 +219,5 @@ plt.plot(np.array(epoch_loss_lst))
 plt.plot(np.array(epoch_loss_lst_val))
 plt.show()
 
-print("--- Program execution done --- time since start [min]",
-      (time.time() - start_time)/60.0)
+print("--- Program execution done --- time since start [h]",
+      (time.time() - start_time)/3600.0)

neuralnetworks/classifiers/las_model.py
@@ -32,13 +32,19 @@ class LasModel(Classifier):
                                  out_weights_std=0.1)
 
         ###Attend and SPELL
-        print('creating attend and spell functions...')
         self.attend_and_spell_cell = AttendAndSpellCell(las_model=self)
 
     def __call__(self, inputs, seq_length, is_training=False, reuse=True,
                  scope=None, targets=None):
+
+        print('\x1b[01;32m'
+              + "Adding LAS conputations, training_graph:", is_training,
+              '\x1b[0m')
+
         #inputs = tf.cast(inputs, self.dtype)
         targets = tf.cast(targets, self.dtype)
+
+        print("las input shape:", tf.Tensor.get_shape(inputs))
 
         if is_training is True:
             assert targets is not None

neuralnetworks/classifiers/layer.py
@@ -129,23 +129,23 @@ def concat(inputs, sequence_lengths, scope):
         sequence_lengths: the length of the input sequences
         scope: the current scope
     Returns:
-        inputs: Concatenated inputs [batch_size, time/2, input_size]
+        inputs: Concatenated inputs [batch_size, time/2, input_size*2]
         sequence_lengths: the lengths of the inputs sequences [batch_size]
     """
-    inputs_lst = tf.unpack(inputs, axis=1, name='plstm_unpack')
-    print(scope + ' initial length ' + str(len(inputs_lst)))
-    print(scope + ' initial shape: ',
-          tf.Tensor.get_shape(inputs_lst[0]))
+    input_shape = tf.Tensor.get_shape(inputs)
+    print(scope + ' initial shape: ', input_shape)
     concat_inputs = []
-    for time_i in range(1, len(inputs_lst), 2):
-        concat_input = tf.concat(1, [inputs_lst[time_i-1],
-                                     inputs_lst[time_i]])
+    for time_i in range(1, int(input_shape[1]), 2):
+        concat_input = tf.concat(1, [inputs[:, time_i-1, :],
+                                     inputs[:, time_i, :]],
+                                 name='plstm_concat')
         concat_inputs.append(concat_input)
 
-    print(scope + ' concat length ' + str(len(concat_inputs)))
-    print(scope + ' concat shape: ',
-          tf.Tensor.get_shape(concat_inputs[0]))
     inputs = tf.pack(concat_inputs, axis=1, name='plstm_pack')
-    sequence_lengths = tf.cast(tf.ceil(sequence_lengths/2),
+
+    concat_shape = tf.Tensor.get_shape(inputs)
+    print(scope + '  concat shape: ', concat_shape)
+
+    sequence_lengths = tf.cast(tf.floor(sequence_lengths/2),
                                tf.int32)
     return inputs, sequence_lengths

neuralnetworks/las_elements.py
@@ -176,7 +176,7 @@ class AttendAndSpellCell(RNNCell):
     def set_features(self, high_lvl_features):
         ''' Set the features when available, storing the features in the
             object makes the cell call simpler.'''
-        print("Feature dimension:", tf.Tensor.get_shape(high_lvl_features))
+        print("   Feature dimension:", tf.Tensor.get_shape(high_lvl_features))
         self.high_lvl_features = tf.unpack(high_lvl_features, axis=1)
 
 
@@ -242,7 +242,7 @@ class AttendAndSpellCell(RNNCell):
             # while the zero states functions create normal dtypes.
             # without the identity op the network unrolling chrashes.
             one_hot_char = tf.identity(one_hot_char)
-            print("shape one_hot_char:", tf.Tensor.get_shape(one_hot_char))
+            print("  shape one_hot_char:", tf.Tensor.get_shape(one_hot_char))
             # The dimension of the context vector is determined by the listener
             # output dimension.
             context_vector = tf.get_variable(